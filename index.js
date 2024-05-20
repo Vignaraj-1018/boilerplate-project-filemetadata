e6f220c663e132efef5a5d66c8ac842b373bf954
Vignaraj D <86393989+Vignaraj-1018@users.noreply.github.com>
2024-05-20 23:00:30 +0530

    commit

--- index.js
+++ index.js
@@ -11,7 +11,16 @@ app.get('/', function (req, res) {
   res.sendFile(process.cwd() + '/views/index.html');
 });
 
-
+var upload = multer({ dest: "uploads/" });
+app.post("/api/fileanalyse", upload.single("upfile"), function(req, res, next) {
+  var upfile = req.file;
+  if (typeof upfile === "undefined") res.json({ error: "file not uploaded" });
+  return res.json({
+    name: upfile.originalname,
+    type: upfile.mimetype,
+    size: upfile.size
+  });
+});
 
 
 const port = process.env.PORT || 3000;
